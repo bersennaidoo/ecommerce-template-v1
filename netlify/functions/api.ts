7254d72132ea108920b1210dc721c1a2f6b32bf5
Bersen Naidoo <bersen.naidoo@gmail.com>
2025-03-29 12:46:56 +0200

    imple

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -26,7 +26,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/products", authenticate, productRouter);
+//app.use("/api/products", authenticate, productRouter);
 app.use("/api/users", userRouter);
 
 app.get('/healthcheck', (req, res) => {
